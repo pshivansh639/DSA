c41c2b3fe12924c181403eac86ef1ade188c063a
Shivansh Pathak <91477357+pshivansh639@users.noreply.github.com>
2023-09-20 14:36:12 +0530

    minmax

--- Array/minmax.java
+++ Array/minmax.java
@@ -1,1 +1,18 @@
-
+import java.util.*;
+class min_max
+{
+    public static void main(String args[])
+    {
+        try (Scanner sc = new Scanner(System.in)) {
+            int n=sc.nextInt();
+            int[] a=new int[n];
+            for(int i=0;i<n;i++)
+            {
+                a[i]= sc.nextInt();
+            }
+            Arrays.sort(a);
+            System.out.println(a[0]);
+            System.out.println(a[n-1]);
+        }
+    }
+}

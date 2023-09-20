ccd44a415bbaeefa6b5676cd0cdd67ff1145877b
Shivansh Pathak <91477357+pshivansh639@users.noreply.github.com>
2023-09-20 14:48:23 +0530

    reverse_array

---
+++ Array/reverse_array.java
@@ -0,0 +1,13 @@
+
+class min_max
+{
+    public static void main(String args[])
+    {
+        int arr[]={3,7,6,4,2,9,8};
+        
+        for(int i=arr.length-1;i>=0;i--)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
